bb82a25d8a5a1a0cb78ffc339a048502ce5660ec
mrwong101 <37117876+mrwong101@users.noreply.github.com>
2018-04-13 18:19:29 +0800

    Add files via upload
    Added scaling

--- KNNv3.py
+++ KNNv3.py
@@ -9,43 +9,59 @@ test_num = -1*int(row_num /3)
 
 col_list = ['side', 'return_t5', "return_t30", "vol_sh_out_pct","stake_pct_chg", "tran_value","mkt_cap", "prev_tran_num","hit_rate_5d", "hit_rate_30d", "hit_rate_90d"]
 
+# Apply Min / Max Scaling
+def scaler(col_name):
+    insider[col_name] = (insider[col_name]-insider[col_name].min())/ (insider[col_name].max()-insider[col_name].min())
+
+scaler_list =  [ 'side', 'return_t5', "return_t30", "vol_sh_out_pct","stake_pct_chg", "tran_value", "mkt_cap", "prev_tran_num","hit_rate_5d", "hit_rate_30d", "hit_rate_90d"]
+
+for i in scaler_list:
+    scaler(i)
+    
 X_train = insider[col_list][:train_num]
 y_train_5d = insider.return_5d[:train_num]
 y_train_30d = insider.return_30d[:train_num]
 y_train_90d = insider.return_90d[:train_num]
 
-X_test = insider[col_list][:test_num]
-y_test_5d = insider.return_5d[:test_num]
-y_test_30d = insider.return_30d[:test_num]
-y_test_90d = insider.return_90d[:test_num]
+X_test = insider[col_list][test_num:]
+y_test_5d = insider.return_5d[test_num:]
+y_test_30d = insider.return_30d[test_num:]
+y_test_90d = insider.return_90d[test_num:]
+
+# Splitting test data into two parts
+X_test1 = X_test[:8620]
+X_test2 = X_test[-8620:]
+y_test_90d1 = y_test_90d[:8620]
+y_test_90d2 = y_test_90d[-8620:]
 
 
 ## Import the Classifier.
 from sklearn.neighbors import KNeighborsClassifier
 ## Instantiate the model with 5 neighbors. 
-knn = KNeighborsClassifier(n_neighbors=5)
+knn = KNeighborsClassifier(n_neighbors=37)
 ## Fit the model on the training data.
 knn.fit(X_train, y_train_5d)
-print knn.score(X_test, y_test_5d)
+print 'KNN score of 5d:', round(knn.score(X_test, y_test_5d)*100,2)
 knn.fit(X_train, y_train_30d)
-print knn.score(X_test, y_test_30d)
+print 'KNN score of 30d:', round(knn.score(X_test, y_test_30d)*100,2)
 knn.fit(X_train, y_train_90d)
-print knn.score(X_test, y_test_90d)
+print 'KNN score of 90d:', round(knn.score(X_test, y_test_90d)*100, 2)
+print 'KNN score of 90d (test1):', round(knn.score(X_test1, y_test_90d1)*100, 2)
+print 'KNN score of 90d (test2):', round(knn.score(X_test2, y_test_90d2)*100, 2)
 
-
-import matplotlib.pyplot as plt  
-import numpy as np
-
-def knn_train(n):
-    knn2 = KNeighborsClassifier(n_neighbors=n)
-    knn2.fit(X_train, y_train_5d)
-    return knn2.score(X_test, y_test_5d)
-
-x = np.arange(1, 20, 1)
-y = []
-for i in x:
-    y.append(knn_train(i))
-
-plt.plot(x, np.asarray(y))
-plt.show()
+#import matplotlib.pyplot as plt  
+#import numpy as np
+#
+#def knn_train(n):
+#    knn2 = KNeighborsClassifier(n_neighbors=n)
+#    knn2.fit(X_train, y_train_90d)
+#    return knn2.score(X_test, y_test_90d)
+#
+#x = np.arange(1, 10, 1)
+#y = []
+#for i in x:
+#    y.append(knn_train(i))
+#
+#plt.plot(x, np.asarray(y))
+#plt.show()
 
